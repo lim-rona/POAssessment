9b994d26eafbf0d49cc5a86bd69c4aa9b0e5af90
Lim Rona <limrona@Lims-MacBook-Pro.local>
2022-03-25 10:02:37 +0800

    first commit

---
+++ src/main/java/vttp2022/ssf/POAssessment/PoAssessmentApplication.java
@@ -0,0 +1,13 @@
+package vttp2022.ssf.POAssessment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PoAssessmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PoAssessmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/vttp2022/ssf/POAssessment/PoAssessmentApplicationTests.java
@@ -0,0 +1,13 @@
+package vttp2022.ssf.POAssessment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PoAssessmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
